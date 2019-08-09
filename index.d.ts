57d7f9f72c226b52900b622825489c8e5a3f11d3
shuufei <digital.shuufei@gmail.com>
2019-08-09 19:02:36 +0900

    Add declare file.

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export * from './dist/fei-components/lib/fei-components.service';
+export * from './dist/fei-components/lib/fei-components.component';
+export * from './dist/fei-components/lib/fei-components.module';
